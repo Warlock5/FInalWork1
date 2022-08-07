62b436f00804daae025caa46216ec647a3cf06cd
warlock <all.exe.warlock@example.com>
2022-08-07 16:30:13 +0300

    Task solved

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -3,3 +3,28 @@
                                 "так", "вот", "тебе", "и", "пожалуйста"};
 string[] array2 = new string[array1.Length];
 
+void SortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SortArray(array1, array2);
+ShowArray(array2);
+
